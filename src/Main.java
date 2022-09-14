7326f25949ed0b719e20bec523ffb9dca04bab37
Djimmka <dimsti94@gmail.com>
2022-09-15 00:17:43 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+
+    }
+}
